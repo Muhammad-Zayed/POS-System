c8f05066c5afe10db3d4081b43f6b2154db4af97
Muhammad Zayed <37517144+Muhammad-Zayed@users.noreply.github.com>
2021-12-09 22:59:06 +0200

    Update site.php

--- resources/lang/en/site.php
+++ resources/lang/en/site.php
@@ -2,4 +2,98 @@
 
 return [
 
+    'login'  => 'Login',
+    'logout' => 'Logout',
+
+
+    'dashboard' => 'Dashboard',
+
+    //Users Related Translations
+    'users' => 'Users',
+    'first_name' => 'First Name',
+    'last_name' => 'Last Name',
+    'email' => 'Email',
+
+    //Categories Related Translations
+    'users' => 'Users',
+    'categories' =>'Categories',
+    'category' =>'Category',
+    'category_name' => 'Category Name',
+    'all_categories' => 'All Categories',
+    'category_count' => 'Number Of Products',
+    'category_list' => 'Categories List',
+
+
+    //Products Related Translations
+
+    'products' =>'Products',
+    'product' =>'Product',
+    'product_name'=>'Product Name',
+    'product_description'=>'Product Description',
+    'purchase_price'=>'Purchase Price',
+    'sell_price'=>'Sell Price',
+    'price'=>'Price',
+    'stock'=>'Quantity',
+    'profit_percent' => 'Win Percenage',
+    'total'         =>'Total',
+
+
+    //clients Related Translations
+    'clients'   => 'Clients',
+    'name'      =>'Name',
+    'client_name'=>'Client Name',
+    'phone'     =>'Phone',
+    'address'   =>'Address',
+    'add_order'=>'Add Order',
+    'orders'=>'Orders',
+    'total_orders'=>'Total Orders',
+    'previous_orders' => 'Previous Orders',
+
+
+
+
+    //MultiLanguage Models Translations
+    'ar'=>[
+        'category_name'  => 'Category Name In Arabic',
+        'product_name'=>'Product Name In Arabic',
+        'product_description'=>'Product Description In Arabic',
+    ],
+
+    'en'=>[
+        'category_name'  => 'Category Name In English',
+        'product_name'=>'Product Name In English',
+        'product_description'=>'Product Description In English',
+    ],
+
+
+    //Actions And Global Words Translations
+    'action' => 'Action',
+    'no_data' => 'No Data Available',
+    'edit' =>'Edit',
+    'delete' => 'Delete',
+    'add' => 'Add',
+    'show' => 'Show',
+    'search' =>'Search',
+    'password' => 'Password',
+    'password_confirmation' => 'Password Confirmation',
+    'added_successfully' => 'Added Successfully',
+    'updated_successfully' => 'Updated Successfully',
+    'deleted_successfully' => 'Deleted Successfully',
+    'permissions' => 'Permissions',
+    'create'=>'Create',
+    'update'=>'Update',
+    'read'=>'Read',
+    'confirm_delete' => 'Are You Sure ?',
+    'yes' => 'Yes',
+    'no' => 'No',
+    'image' => 'Image',
+    'remember_me' => 'Remember Me',
+    'welcome' => 'Welcome',
+    'created_at' => 'Order Date',
+    'loading'   =>'Loading',
+    'print'     =>'Print',
+    'Quantity_Is_Invalid'=>'Invalid Quantity',
+    'order_required'    => 'Most Be At Least One Product',
+    'minimum_product_quantity'    => 'Invalid Quantity',
+
 ];
